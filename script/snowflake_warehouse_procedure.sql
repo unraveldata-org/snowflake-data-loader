281236c37213032ec8040c7c2a93581ef1741868
govindunravel <gmishra@unraveldata.com>
2024-01-12 17:32:40 +0530

    SNOW-1520 : WAREHOUSES_PARAMETERS Table is not shared with Unravel SF account via sql command Sharing

--- script/snowflake_warehouse_procedure.sql
+++ script/snowflake_warehouse_procedure.sql
@@ -5,9 +5,10 @@ CREATE OR REPLACE PROCEDURE warehouse_proc(dbname STRING, schemaname STRING)
 AS
 $$
 
-function logError(err)
+var warehouse_proc_task = "warehouse_proc ---> Warehouses and Warehouse_Parameter Table Creation";
+function logError(err, taskName)
 {
-    var fail_sql = "INSERT INTO REPLICATION_LOG VALUES (current_date,'FAIL', " + "'"+ err +"'"+");" ;
+    var fail_sql = "INSERT INTO REPLICATION_LOG VALUES (current_timestamp,'FAILED', "+"'"+ err +"'"+", "+"'"+ taskName +"'"+");" ;
     sql_command1 = snowflake.createStatement({sqlText: fail_sql} );
     sql_command1.execute();
 }
@@ -20,7 +21,7 @@ try {
 	stmt.execute();
 	result = "Database: " + DBNAME + " creation is success";
 } catch (err) {
-     logError(err)
+     logError(err, warehouse_proc_task);
 	return "Failed to create DB " + DBNAME + ", error: " + err;
 }
 
@@ -32,75 +33,105 @@ try {
 	stmt.execute();
 	result += "\nSchema: " + SCHEMANAME + " creation is success";
 } catch (err) {
-    logError(err)
+    logError(err, warehouse_proc_task);
 	return "Failed to create the schema " + SCHEMANAME + ", error: " + err;
 }
 
-var showWarehouse = 'SHOW WAREHOUSES;';
-var createWarehouseTable = 'CREATE OR REPLACE TRANSIENT TABLE  ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSES AS SELECT * FROM TABLE(result_scan(last_query_id()));';
+
 var returnVal = "SUCCESS";
 var error = "";
 
 try {
-	var warehouseStmt = snowflake.createStatement({
+    // 1. create warehouse table if not exist
+    var createWarehouseTable = 'CREATE TRANSIENT TABLE IF NOT EXISTS ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSES(NAME VARCHAR(16777216), STATE VARCHAR(16777216), TYPE VARCHAR(16777216), SIZE VARCHAR(16777216), MIN_CLUSTER_COUNT NUMBER(38,0), MAX_CLUSTER_COUNT NUMBER(38,0), STARTED_CLUSTERS NUMBER(38,0), RUNNING NUMBER(38,0), QUEUED NUMBER(38,0), IS_DEFAULT VARCHAR(1), IS_CURRENT VARCHAR(1), AUTO_SUSPEND NUMBER(38,0), AUTO_RESUME VARCHAR(16777216), AVAILABLE VARCHAR(16777216), PROVISIONING VARCHAR(16777216), QUIESCING VARCHAR(16777216), OTHER VARCHAR(16777216), CREATED_ON TIMESTAMP_LTZ(9), RESUMED_ON TIMESTAMP_LTZ(9), UPDATED_ON TIMESTAMP_LTZ(9), OWNER VARCHAR(16777216), COMMENT VARCHAR(16777216), ENABLE_QUERY_ACCELERATION VARCHAR(16777216), QUERY_ACCELERATION_MAX_SCALE_FACTOR NUMBER(38,0), RESOURCE_MONITOR VARCHAR(16777216), ACTIVES NUMBER(38,0), PENDINGS NUMBER(38,0), FAILED NUMBER(38,0), SUSPENDED NUMBER(38,0), UUID VARCHAR(16777216), SCALING_POLICY VARCHAR(16777216), BUDGET VARCHAR(16777216));';
+
+
+var createWarehouseTableStmt = snowflake.createStatement({
+		sqlText: createWarehouseTable
+	});
+    createWarehouseTableStmt.execute();
+
+    // 2. truncate table
+    var truncateWarehouse = 'TRUNCATE TABLE IF EXISTS ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSES;';
+    var truncateWarehouseStmt = snowflake.createStatement({
+		sqlText: truncateWarehouse
+	});
+    truncateWarehouseStmt.execute();
+
+
+   // 3. run show warehouses
+    var showWarehouse = 'SHOW WAREHOUSES;';
+	var showWarehouseStmt = snowflake.createStatement({
 		sqlText: showWarehouse
 	});
-	var resultSet = warehouseStmt.execute();
-	// Checking if the SHOW WAREHOUSES statement returned any rows before creating the table
-	if (resultSet.next() == false) {
-		error += "No warehouses found.";
-	} else {
-		var warehouseTableStmt = snowflake.createStatement({
-			sqlText: createWarehouseTable
+    var resultSet = showWarehouseStmt.execute();
+
+    // 4. insert to warehouse
+    var insertToWarehouse = 'INSERT INTO ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSES  SELECT * FROM TABLE(result_scan(last_query_id()));';
+    var insertToWarehouseStmt = snowflake.createStatement({
+			sqlText: insertToWarehouse
 		});
-		warehouseTableStmt.execute();
-	}
+	insertToWarehouseStmt.execute();
+
 } catch (err) {
-	logError(err)
+	logError(err, warehouse_proc_task);
     error += "Failed: " + err;
 }
 
 try {
-	var createWP = 'CREATE OR REPLACE TRANSIENT TABLE  ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSE_PARAMETERS (WAREHOUSE VARCHAR(1000), KEY VARCHAR(1000), VALUE VARCHAR(1000), DEFUALT VARCHAR(1000),LEVEL VARCHAR(1000), DESCRIPTION VARCHAR(10000),TYPE VARCHAR(100));';
+
+    //1. create warehouse parameters table
+	var createWP = 'CREATE TRANSIENT TABLE IF NOT EXISTS ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSE_PARAMETERS (WAREHOUSE VARCHAR(1000), KEY VARCHAR(1000), VALUE VARCHAR(1000), DEFUALT VARCHAR(1000),LEVEL VARCHAR(1000), DESCRIPTION VARCHAR(10000),TYPE VARCHAR(100));';
 
 	var createWPStmt = snowflake.createStatement({
 		sqlText: createWP
 	});
 	createWPStmt.execute();
+
+    //2. trunate warehouse parameter tables
+    var truncateWarehouseParameter = 'TRUNCATE TABLE IF EXISTS ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSE_PARAMETERS;';
+    var truncateWarehouseParameterStmt = snowflake.createStatement({
+		sqlText: truncateWarehouseParameter
+	});
+    truncateWarehouseParameterStmt.execute();
+
 } catch (err) {
-	logError(err)
+	logError(err, warehouse_proc_task);
     error += "Failed: " + err;
 }
-var showWP = '';
-try {
 
+
+try {
+    //3.Get warehouse details
 	var wn = 'SELECT * FROM ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSES;';
 	var wnStmt = snowflake.createStatement({
 		sqlText: wn
 	});
 	var resultSet1 = wnStmt.execute();
 	while (resultSet1.next()) {
-		var whName = resultSet1.getColumnValue('name');
-
-		showWP = 'SHOW PARAMETERS IN WAREHOUSE ' + whName + ';';
-
+		var whName = resultSet1.getColumnValue(1);
+       //4. show warehouse parameters
+		var showWP = 'SHOW PARAMETERS IN WAREHOUSE ' + whName + ';';
 		var showWPStmt = snowflake.createStatement({
 			sqlText: showWP
 		});
 		showWPStmt.execute();
 
+        //5. insert into WAREHOUSE_PARAMETERS table
 		var wpInsert = 'INSERT INTO ' + DBNAME + '.' + SCHEMANAME + '.WAREHOUSE_PARAMETERS SELECT ' + "'" + whName + "'" + ',* FROM TABLE (result_scan(last_query_id()));';
 
-		var wpInsertStmt = snowflake.createStatement({
+        var wpInsertStmt = snowflake.createStatement({
 			sqlText: wpInsert
 		});
 		wpInsertStmt.execute();
 
-	}
+        }
+
+
 } catch (err) {
 
   error += "Failed: " + err;
-  return logError(err)
+  return logError(err, warehouse_proc_task);
 
 }
 
